35f546ac988651b3bd99e2a84687b3c764d3638f
morganika <butusova.ulia@inbox.ru>
2018-10-22 23:25:59 +0300

    Update имяадресс.cpp

---
+++ имяадресс.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h> 
+#include <locale.h> 
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+setlocale(0, ""); 
+cout<<"Бутусова Юля"<<endl; 
+cout<<"Санкт-Петербург, проспект Мира,8"; 
+system("pause"); 
+return 0; 
+}
+
